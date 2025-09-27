04dde5adc04412b3a05d7f762914692e02a750d4
9cm1 <micheloconrad@gmail.com>
2025-09-27 22:39:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,41 @@
-const CACHE_NAME = '9groceries-v1';
+const CACHE_NAME = '9groceries-v2'; // ← CHANGE VERSION NUMBER
 const urlsToCache = [
-  '/9groceries/',
-  '/9groceries/index.html',
-  '/9groceries/manifest.json',
-  '/9groceries/icons/icon-192.png',
-  '/9groceries/icons/icon-512.png',
-  '/9groceries/sw.js'
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 // Install event - cache files
 self.addEventListener('install', event => {
+  self.skipWaiting(); // ← ADD THIS LINE
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Activate event - clean up old caches
+self.addEventListener('activate', event => { // ← ADD THIS NEW EVENT
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  event.waitUntil(self.clients.claim()); // ← ADD THIS LINE
+});
+
 // Fetch event - serve from cache
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Return cached version or fetch from network
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
