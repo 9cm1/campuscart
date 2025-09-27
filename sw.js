f30bba72b17d4f2d35c3a0d8f047c242de13dee8
9cm1 <micheloconrad@gmail.com>
2025-09-27 23:02:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,41 @@
-const CACHE_NAME = '9groceries-v2'; // ← CHANGE VERSION NUMBER
+const CACHE_NAME = '9groceries-v3';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+    './',
+    './index.html',
+    './manifest.json',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
 ];
 
 // Install event - cache files
 self.addEventListener('install', event => {
-  self.skipWaiting(); // ← ADD THIS LINE
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
 // Activate event - clean up old caches
-self.addEventListener('activate', event => { // ← ADD THIS NEW EVENT
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
-  event.waitUntil(self.clients.claim()); // ← ADD THIS LINE
+    );
+    event.waitUntil(self.clients.claim());
 });
 
 // Fetch event - serve from cache
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
