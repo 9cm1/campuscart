0e1c09a9ae6e9b2e066ae56b421b41e5dc4bf8e2
9cm1 <micheloconrad@gmail.com>
2025-11-11 18:24:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,41 @@
-const CACHE_NAME = '9groceries-v3';
+const CACHE_NAME = 'campus-cart-v1';
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './icons/icon-192.png',
-    './icons/icon-512.png'
+  '/campuscart/',
+  '/campuscart/index.html',
+  '/campuscart/manifest.json',
+  '/campuscart/icons/icon-192.png',
+  '/campuscart/icons/icon-512.png'
 ];
 
 // Install event - cache files
 self.addEventListener('install', event => {
-    self.skipWaiting();
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 // Activate event - clean up old caches
 self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
         })
-    );
-    event.waitUntil(self.clients.claim());
+      );
+    })
+  );
+  event.waitUntil(self.clients.claim());
 });
 
 // Fetch event - serve from cache
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
